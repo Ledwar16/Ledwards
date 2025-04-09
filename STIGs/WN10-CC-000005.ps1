e2ae62ce2e4fbc2522e356799187c86fa4b4027e
LaDarius Edwards <147221025+Ledwar16@users.noreply.github.com>
2025-04-09 01:51:37 -0400

    Update WN10-CC-000005.ps1

--- STIGs/WN10-CC-000005.ps1
+++ STIGs/WN10-CC-000005.ps1
@@ -1,3 +1,29 @@
+<#
+.SYNOPSIS
+    This script configures Windows audit policies to enable failure auditing for User Account Management, ensuring system security and event logging.
+
+.NOTES
+    Author          : LaDarius Edwards
+    LinkedIn        : www.linkedin.com/in/ladarius-edwards
+    GitHub          : https://github.com/Ledwar16/Ledwards/
+    Date Created    : 2024-04-09
+    Last Modified   : 2025-04-09
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000005
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Example syntax:
+    PS C:\> .\WN10-CC-000005.ps1 
+#>
+
 # Ensure running as Administrator
 if (-not (Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies")) {
     Write-Error "Please run PowerShell as Administrator!"

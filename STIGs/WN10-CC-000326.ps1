7edb40e32e491fd393efcab70e863eb779045969
LaDarius Edwards <147221025+Ledwar16@users.noreply.github.com>
2025-04-10 00:22:33 -0400

    Create WN10-CC-000326.ps1

---
+++ STIGs/WN10-CC-000326.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+This script enables PowerShell Script Block Logging by setting the EnableScriptBlockLogging registry key to 1, creates the necessary registry path if it doesn't exist, forces a Group Policy update to apply the changes, and then verifies the updated configuration..NOTES
+    Author          : LaDarius Edwards
+    LinkedIn        : www.linkedin.com/in/ladarius-edwards
+    GitHub          : https://github.com/Ledwar16/Ledwards/
+    Date Created    : 2024-04-10
+    Last Modified   : 2025-04-10
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000326
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Example syntax:
+    PS C:\> .\WN10-CC-000326.ps1 
+#>
+
+# Define registry path
+$RegPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"
+
+# Ensure the registry path exists
+If (!(Test-Path $RegPath)) {
+    New-Item -Path $RegPath -Force
+}
+
+# Enable PowerShell Script Block Logging
+Set-ItemProperty -Path $RegPath -Name "EnableScriptBlockLogging" -Value 1 -Type DWord
+
+# Force Group Policy update to apply changes
+gpupdate /force
+
+# Verify the configuration
+Get-ItemProperty -Path $RegPath -Name "EnableScriptBlockLogging"
